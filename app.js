2f34001d26c3a5ad764b7dfa4d10bdce3989419e
keeganthomp <whereiskeegan@gmail.com>
2017-06-23 12:35:01 -0500

    server is up and running

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const mustachExpress = require("mustache-express");
+const fs = require('fs')
 const port = process.env.PORT || 9000;
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const sessionConfig = require("./sessionConfig");
 
-var users = [];
+
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
 //set view engine
 app.engine("mustache", mustachExpress());
@@ -15,6 +18,7 @@ app.set("view engine", "mustache");
 //middleware
 app.use("/", express.static("./views"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
 function checkAuth(req, res, next) {
   if (!req.session.user) {
     res.redirect("/login");
@@ -22,3 +26,11 @@ function checkAuth(req, res, next) {
     next();
   }
 }
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.listen(port, function() {
+  console.log("Application is running on", port);
+});
---
+++ sessionConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  secret: "blue lock rock",
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 900000 }
+};
