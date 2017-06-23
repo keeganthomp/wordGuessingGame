3e2eabc0cd8ca4efc2a5da29f1300b8ac2dd47d9
keeganthomp <whereiskeegan@gmail.com>
2017-06-23 15:39:54 -0500

    updating logic to populate page with appropriate spaces and shows users guesses

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const mustachExpress = require("mustache-express");
-const fs = require('fs')
+const fs = require("fs");
 const port = process.env.PORT || 9000;
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const sessionConfig = require("./sessionConfig");
 
-
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const words = fs
+  .readFileSync("/usr/share/dict/words", "utf-8")
+  .toLowerCase()
+  .split("\n");
 
 //set view engine
 app.engine("mustache", mustachExpress());
@@ -19,16 +21,54 @@ app.set("view engine", "mustache");
 app.use("/", express.static("./views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
-function checkAuth(req, res, next) {
-  if (!req.session.user) {
-    res.redirect("/login");
-  } else {
-    next();
-  }
-}
 
 app.get("/", function(req, res) {
-  res.render("index");
+  var randomWord = words[Math.floor(Math.random() * words.length)];
+  console.log("randomWord", randomWord);
+  var randomWordArray = randomWord.split("");
+  var dashedArray = [];
+  var guessedLetter = "";
+  for (i = 0; i < randomWordArray.length; i++) {
+    randomWordArray[i] = "_";
+    dashedArray.push(randomWordArray[i]);
+  }
+
+  res.render("index", {
+    computerWord: dashedArray,
+    guessedLetter: guessedLetter,
+    guessCount: guessCount
+  });
+});
+
+app.post("/", function(req, res) {
+  var guessCount = 8;
+  userGuess = req.body;
+  if (
+    userGuess.guessedLetter.length > 1 ||
+    userGuess.guessedLetter.length <= 0
+  ) {
+    return res.redirect("/");
+  } else if (randomWord.indexOf(userGuess.guessedLetter >= 0)) {
+    guessedLetter = userGuess.guessedLetter;
+    for (let i = 0; i < randomWord.length; i++) {
+      if (randomWord[i] == letter) {
+        dashedArray[i] = letter;
+      }
+    }
+    guessCount -= 1;
+    console.log("guess counter:", guessCount);
+    if (guessCount <= 0) {
+      guessCount = 8;
+      return res.render("index");
+    }
+    return res.redirect("/");
+  } else if (computerWord.indexOf("_") == -1) {
+    res.redirect("/winner");
+  }
+});
+
+app.get("/winner", function(req, res) {
+  res.render("winner");
 });
 
 app.listen(port, function() {
