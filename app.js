3e80e8158722b08bc642535314b19c554910db6d
keeganthomp <whereiskeegan@gmail.com>
2017-06-25 14:01:32 -0500

    finalizing logic and added basic styling

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(session(sessionConfig));
 
 app.get("/", function(req, res) {
   if (randomWordArray.toString() === dashedArray.toString()) {
-    return res.render("winner", {randomWord: randomWord});
+    return res.render("winner", { randomWord: randomWord });
   }
   res.render("index", {
     computerWord: dashedArray,
@@ -62,39 +62,36 @@ app.get("/", function(req, res) {
 var guessCount = 8;
 
 app.post("/", function(req, res) {
-  console.log("random-word:", randomWord);
   userGuess = req.body;
-  if (randomWord.indexOf(userGuess.guessedLetter) >= 0) {
-    guessedLetter = userGuess.guessedLetter.toLowerCase();
+  if (userGuesses.indexOf(userGuess.guessedLetter) >= 0) {
+    errorMessage =
+      "You already guessed '" +
+      userGuess.guessedLetter +
+      "', guess a different letter";
+  } else if (randomWord.indexOf(userGuess.guessedLetter) >= 0) {
+    guessedLetter = userGuess.guessedLetter;
     for (let i = 0; i < randomWordArray.length; i++) {
       if (randomWordArray[i] == guessedLetter) {
         dashedArray[i] = guessedLetter;
-        userGuesses.push(userGuess.guessedLetter);
       }
     }
+    userGuesses.push(guessedLetter);
     guessCount -= 1;
-  } else if (userGuess.guessedLetter === null) {
-    res.redirect("/");
   } else if (
     userGuess.guessedLetter.length > 1 ||
     userGuess.guessedLetter.length <= 0 ||
     userGuess.guessedLetter.indexOf(" ") >= 0
   ) {
-    console.log("too many");
-    errorMessage = "You may only guess one letter at a time and no spaces";
-    return res.redirect("/");
-  } else if (userGuesses.indexOf(userGuess.guessedLetter) >= 0) {
-    console.log("Already guessed it");
     errorMessage =
-      "You already guessed '" +
-      userGuess.guessedLetter +
-      "', guess a different letter";
+      "You may only guess one letter at a time. No spaces. No Numbers";
+    return res.redirect("/");
   } else if ((userGuess.guessedLetter.length = 1)) {
     guessCount -= 1;
     userGuesses.push(userGuess.guessedLetter);
-  } else if (guessCount <= 0) {
+  }
+  if (guessCount <= 0) {
     guessCount = 8;
-    return res.redirect("/");
+    return res.render("fail", { randomWord: randomWord });
   }
 
   res.redirect("/");
--- views/main.js
+++ views/main.js
@@ -2,9 +2,12 @@
     var error = document.querySelector(".error");
     var gotItBtn = document.querySelector("#confirm");
     var game = document.querySelector(".game");
+    var gif = document.querySelector(".mainGif");
     if(error.innerHTML != ""){
         gotItBtn.classList.remove("hidden");
         game.classList.add("hidden");
+        gif.classList.add("hidden");
+
     }
 })();
 
